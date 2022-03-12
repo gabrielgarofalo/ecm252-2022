090edc4fdbbe2e2466e4fe1b85ea913d12b7f608
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-03-12 12:26:04 -0300

    url montada

--- aula4/aula4.js
+++ aula4/aula4.js
@@ -1,1 +1,14 @@
-https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=b65fa48e5e479549dfcb02c057a362f2
+require('dotenv').config()
+
+//const PROTOCOL = process.env.PROTOCOL
+//const BASE_URL = process.env.BASE_URL
+//const APP_ID = process.env.APP_ID
+//const LANGUAGE = process.env.LANGUAGE
+//const UNITS = process.env.UNITS
+
+// Operador de desestruturação
+
+const {PROTOCOL, BASE_URL, LANGUAGE, APP_ID, UNITS, Q} = process.env
+
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APP_ID}&units=${UNITS}&lang=${LANGUAGE}&q=${Q}`
+console.log(url)
