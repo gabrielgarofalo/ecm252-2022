5393e1021195f786c6e479345a01c7ba694c0153
gabrielgarofalo <gabrieldgarofalo@gmail.com>
2022-05-14 09:33:52 -0300

    cont microserv

--- memos/lembretes/index.js
+++ memos/lembretes/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 const lembretes = {};
@@ -9,13 +10,27 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 });
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     contador ++;
     const {texto} = req.body;
     lembretes[contador] = {
         contador, texto
     }
+
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        }
+    });
+
     res.status(200).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
 });
 
 app.listen(4000, () => {
--- memos/observacoes/index.js
+++ memos/observacoes/index.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
-
+const axios = require('axios');
 const observacoesPorLembreteId = {}
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/lembretes/:id/observacoes', (req, res) => {
+app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4();
-    const {texto} = req.body;
+    const { texto } = req.body;
 
-    const observacoesDoLembrete = 
+    const observacoesDoLembrete =
         observacoesPorLembreteId[req.params.id] || [];
-    observacoesDoLembrete.push({id: idObs, texto});
+    observacoesDoLembrete.push({ id: idObs, texto });
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs, texto, lembreteId: req.params.id
+        }
+    })
+
     res.status(201).send(observacoesDoLembrete);
 });
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
     res.send(observacoesPorLembreteId[req.params.id] || []);
 });
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
 
 app.listen(5000, () => {
     console.log('Lembretes. Porta 5000.')
